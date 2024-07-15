f52b98ab8dfdb40729c5d0d3af10ca3de30c6e09
Abhinav-bM <abhinavbmanoj@gmail.com>
2024-07-15 09:42:05 +0530

    update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 connectDB();
 
 // MIDDLEWARE
-app.use(cors({ origin: "https://real-estate-react-app-phi.vercel.app", credentials: true }));
+app.use(cors())
 app.use(bodyParser.json());
 app.use(cookieParser());
 
